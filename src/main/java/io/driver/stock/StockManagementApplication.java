a08595f38937aa85e5e46a5ecfedfb4b9c656a29
JAEHEE25 <y2hjjh@naver.com>
2024-10-30 17:59:19 +0900

    Initial Commit

---
+++ src/main/java/io/driver/stock/StockManagementApplication.java
@@ -0,0 +1,13 @@
+package io.driver.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementApplication.class, args);
+	}
+
+}
