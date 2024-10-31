f1a21f2d6aaa30e7a92f6d904b681985e924de5f
JAEHEE25 <y2hjjh@naver.com>
2024-10-31 23:57:19 +0900

    [#3] 편의 기능
    - 필터링

--- src/main/java/io/driver/stock/modules/stock/domain/StockRepository.java
+++ src/main/java/io/driver/stock/modules/stock/domain/StockRepository.java
@@ -1,8 +1,11 @@
 package io.driver.stock.modules.stock.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+	List<Stock> findAllByStockCategory(StockCategory category);
 }
